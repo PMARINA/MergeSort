5a7b82fb5c555e7648c9037fc3b2f4fed12180f3
Pridhvi Myneni <bluerat33@gmail.com>
2016-10-17 21:40:28 -0400

    Finished location()

--- MergeSort.java
+++ MergeSort.java
@@ -5,14 +5,17 @@ public class MergeSort implements Sorter {
 	private static int boundry;
 	@Override
 	public int[] location() {
-		if(boundry == 0){
-			System.out.println("Boundries have not yet been set");
-		}
-		if(x>0)x-=1;
-		else{
-			x = boundry; y-=1;
-		}
-		return new int[]{x,y};
+//If the head moves around between calls of location()
+//		if(boundry == 0){
+//			System.out.println("Boundries have not yet been set");
+//		}
+//		if(x>0)x-=1;
+//		else{
+//			x = boundry; y-=1;
+//		}
+//		return new int[]{x,y};
+//If the head is stationary, and we need to look for it
+		return new int[]{(int)(Math.random() * boundry),(int)(Math.random() * boundry)};
 	}
 
 	@Override
@@ -24,6 +27,18 @@ public class MergeSort implements Sorter {
 	public void setBoundaries(int side) {
 		boundry=side;
 		y = x = boundry;
+	}
+
+	@Override
+	public void drawSorter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawFinal() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
