dc1e8066835a608ecdc7ff02671c85f16508ba96
Pridhvi Myneni <bluerat33@gmail.com>
2016-10-17 23:06:40 -0400

    Added Simran's Work, Attempted to make Simran's work work with the Interface. Failed. Not enough documentation provided by TokenKeepers.

--- MergeSort.java
+++ MergeSort.java
@@ -1,50 +1,71 @@
 
-
 public class MergeSort implements Sorter {
 	private static int x;
 	private static int y;
 	private static int boundry;
+	private static Node first;
+
 	@Override
+
+	public MergeSort() {
+		first = null;
+	}
+
 	public int[] location() {
-//If the head is stationary
-//		if(boundry == 0){
-//			System.out.println("Boundries have not yet been set");
-//		}
-//		if(x>0)x-=1;
-//		else{
-//			x = boundry; y-=1;
-//		}
-//		return new int[]{x,y};
-//If the head moves around between calls of location()
-		x = (int)(Math.random() * boundry);
-		y = (int)(Math.random() * boundry);
-		if(boundry == 0)System.out.println("Boundries have not yet been set");
-		return new int[]{x,y};
+		// If the head is stationary
+		// if(boundry == 0){
+		// System.out.println("Boundries have not yet been set");
+		// }
+		// if(x>0)x-=1;
+		// else{
+		// x = boundry; y-=1;
+		// }
+		// return new int[]{x,y};
+		// If the head moves around between calls of location()
+		x = (int) (Math.random() * boundry);
+		y = (int) (Math.random() * boundry);
+		if (boundry == 0)
+			System.out.println("Boundries have not yet been set");
+		return new int[] { x, y };
 	}
 
 	@Override
 	public LinkedList sort(LinkedList aList) {
-		return null;
+		// Somehow convert aList to a node, to actually make it a linked list,
+		// or somehow get the head, make this bList
+		Node bList = null; // See previous comment
+		MergeSortForLinkedList a = new MergeSortForLinkedList();
+		a.append(bList);
+		Node cList = a.MergeSort(bList);// Sorted now
+		// Somehow convert between cList (node) and LinkedList, call this dList
+		LinkedList dList = null;// See previous comment
+		return dList;
 	}
 
 	@Override
 	public void setBoundaries(int side) {
-		boundry=side;
+		boundry = side;
 		y = x = boundry;
 	}
 
 	@Override
 	public void drawSorter() {
-		StdDraw.setCanvasSize(boundry,boundry);
+		StdDraw.setCanvasSize(boundry, boundry);
 		StdDraw.setPenColor();
 		StdDraw.point(x, y);
-		
+
 	}
 
 	@Override
 	public void drawFinal() {
-		// TODO Auto-generated method stub
-		
+		// Have no idea how to do this. Need info on how to draw the image
+		// through standard draw, as Token Keepers are not using StdDraw, but
+		// rather JFrame.
+		// What I plan on doing:
+		// iterate through the linked list, using its payload (hopefully a path)
+		// to find an image corresponding to the node, which I will then draw
+		// using stdDraw.picture();.
+
 	}
-	
+
 }
--- Node.java
+++ Node.java
@@ -1,60 +1,16 @@
-
-/**
- * A Node
- * 
- * @author PMARINA
- * @version 10/3/2016
- */
 public class Node {
-	private String item; /// The item represented by the node
-	private Node next; // The next node in the queue
+	public int item;
+	public Node next;
 
-	/**
-	 * @param item
-	 *            the item held in the Node (String)
-	 * @param n
-	 *            the next node
-	 */
-	Node(String item, Node n) {
-		setItem(item);
-		setNext(n);
+	public Node(int val) {
+		item = val;
 	}
 
-	/**
-	 * @param item
-	 *            the item to set the item in the node to
-	 */
-	public void setItem(String item) {
-		this.item = item;
+	public Node() {
+
 	}
 
-	/**
-	 * @param next
-	 *            the Node to set the next node as
-	 */
-	public void setNext(Node next) {
-		this.next = next;
-	}
-
-	/**
-	 * @return the item represented by the node
-	 */
-	public String getItem() {
-		return item;
-	}
-
-	/**
-	 * @return the next node
-	 */
-	public Node getNext() {
-		return next;
-	}
-
-	/**
-	 * @return whether or not this node has another node as part of the next
-	 *         node
-	 */
-	public boolean hasNext() {
-		return next != null;
+	public void printNode() {
+		System.out.println("[" + item + "] ");
 	}
 }
